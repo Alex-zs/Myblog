88c672449f178d7d849659c20759efdea8d5a6b9
Alex <alex-zs@foxmail.com>
2018-04-22 22:23:35 +0800

    文章

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Article
 
+admin.site.site_header = '陈开拓个人博客管理'
+admin.site.site_title = '后台管理'
+
 class ArticleAdmin(admin.ModelAdmin):
 	list_display = ('title','pub_date','update_time',)
+	fieldsets = (
+		[u'',{
+		'fields':('title','content','article_cate',)
+		}],
+		)
 
 
 		
